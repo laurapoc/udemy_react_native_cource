0dd1f7e7201345a5dac7e608c718e8eadbf2badb
Laura Pociuniene <laurapociuniene@gmail.com>
2023-10-13 17:33:19 +0300

    Create first list of course goals

--- App.js
+++ App.js
@@ -1,30 +1,66 @@
+import { useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [courceGoals, setCourceGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoalText(enteredText);
+  };
+
+  const addGoaldHandler = () => {
+    console.log(enteredGoalText);
+    setCourceGoals((prevState) => [...prevState, enteredGoalText]);
+    setEnteredGoalText("");
+  };
+
+  const deleteGoalHandler = () => {
+    setCourceGoals([]);
+  };
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your cource goal!" />
-        <Button title="Add goal" />
+        <TextInput
+          value={enteredGoalText}
+          style={styles.textInput}
+          placeholder="Your cource goal!"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add goal" onPress={addGoaldHandler} />
       </View>
-      <View>
-        <Text>List of goals...</Text>
+      <View style={styles.goalsCOntainer}>
+        {courceGoals.map((item) => (
+          <Text key={item}>{item}</Text>
+        ))}
+        {courceGoals.length > 0 && (
+          <Button title="Delete all goals" onPress={deleteGoalHandler} />
+        )}
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  appContainer: { padding: 50 },
+  appContainer: { flex: 1, paddingTop: 50, paddingHorizontal: 16 },
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
   },
   textInput: {
     borderWidth: 1,
     borderColor: "#cccccc",
-    width: "80%",
+    width: "70%",
     marginRight: 8,
-    padding: 8
+    padding: 8,
+  },
+  goalsCOntainer: {
+    flex: 5,
   },
 });
